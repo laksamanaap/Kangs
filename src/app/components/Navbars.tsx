f5fb3ee886f2f1fcb7fcf8c64cf71ad5e5c930cf
laksamanaap <laksamana.arya1412@gmail.com>
2024-05-07 16:42:00 +0700

    feat : change navbar

--- src/app/components/Navbars.tsx
+++ src/app/components/Navbars.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
@@ -11,69 +12,54 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import inaFlag from "@/assets/indonesia.png";
 import ukFlag from "@/assets/united-kingdom.png";
 import chinaFlag from "@/assets/china.png";
+import logo from "@/assets/Logo KANGS (Black).png";
 
 const Navbars = () => {
+    const [selectedLanguage, setSelectedLanguage] = useState("English");
+    const [selectedFlag, setSelectedFlag] = useState(ukFlag);
+
+    const handleLanguageChange = (language: any) => {
+        setSelectedLanguage(language);
+        switch (language) {
+            case "Indonesia":
+                setSelectedFlag(inaFlag);
+                break;
+            case "China":
+                setSelectedFlag(chinaFlag);
+                break;
+            default:
+                setSelectedFlag(ukFlag);
+                break;
+        }
+    };
+
+
     return (
-        // <nav className="navbar navbar-expand-lg" id="navbar">
-        //     <span className="navbar-brand fw-medium">Kang<span className="navbar-brand active">s.</span></span>
-        //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="#navbarCollapse" aria-expanded="false" aria-label="Toggle Navigation">
-        //         <span className="navbar-toggler-icon"></span>
-        //     </button>
-        //     <div className="collapse navbar-collapse" id="navbarCollapse">
-        //         <ul className="navbar-nav d-flex gap-3 mx-auto mg-lg-0">
-        //             <li>
-        //                 <a className="nav-link" aria-current="page" href="#">Package</a>
-        //             </li>
-        //             <li>
-        //                 <a className="nav-link" aria-current="page" href="#">Services</a>
-        //             </li>
-        //             <li>
-        //                 <a className="nav-link" aria-current="page" href="#">Blog</a>
-        //             </li>
-        //             <li>
-        //                 <a className="nav-link" aria-current="page" href="#">FAQ</a>
-        //             </li>
-        //         </ul>
-        //         <div className="d-flex gap-3 align-items-center">
-        //             <Dropdown>
-        //                 <Dropdown.Toggle className='primary text-white' id="dropdown-basic">
-        //                     <Image src={inaFlag} width={24} height={24} alt="Indonesia Flag" />
-        //                 </Dropdown.Toggle>
-        //                 <Dropdown.Menu className='text-center'>
-        //                     <Dropdown.Item href="#/action-1">
-        //                         <Image src={inaFlag} width={24} height={24} alt="Indonesia Flag" />
-        //                     </Dropdown.Item>
-        //                     <Dropdown.Item href="#/action-2">
-        //                         <Image src={ukFlag} width={24} height={24} alt="UK Flag" />
-        //                     </Dropdown.Item>
-        //                     <Dropdown.Item href="#/action-3">
-        //                         <Image src={chinaFlag} width={24} height={24} alt="China Flag" />
-        //                     </Dropdown.Item>
-        //                 </Dropdown.Menu>
-        //             </Dropdown>
-        //         </div>
-        //     </div>
-        // </nav>
-        <Navbar expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">
-                            Another action
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">
-                            Separated link
-                        </NavDropdown.Item>
-                    </NavDropdown>
+        <Navbar expand="lg" className="justify-content-between pt-0">
+            <Navbar.Brand href="#home">
+                <Image src={logo} alt="Kangs Logo" width={60} height={60} className='img-fluid d-block mx-auto' />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+                <Nav className="mx-auto gap-3 fw-medium">
+                    <Nav.Link href="#home">Packages</Nav.Link>
+                    <Nav.Link href="#link">Services</Nav.Link>
+                    <Nav.Link href="#link">Blog</Nav.Link>
+                    <Nav.Link href="#link">FAQ</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
+            <Nav className="align-items-center">
+                <Nav.Item>
+                    <Nav.Link href="#language" className="d-flex align-items-center">
+                        <Image src={selectedFlag} width={24} height={24} alt="Selected Flag" />
+                    </Nav.Link>
+                </Nav.Item>
+                <NavDropdown title={selectedLanguage} id="basic-nav-dropdown" onSelect={handleLanguageChange}>
+                    <NavDropdown.Item eventKey="English">English</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="Indonesia">Indonesia</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="China">China</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
         </Navbar>
     )
 }
