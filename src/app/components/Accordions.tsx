b6a72ee34ab648a5875f966921ed20670018ce39
laksamanaap <laksamana.arya1412@gmail.com>
2024-05-11 18:56:48 +0700

    feat : faq section

---
+++ src/app/components/Accordions.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Accordion from 'react-bootstrap/Accordion';
+
+const Accordions = () => {
+
+    interface FaqProps {
+        id: number,
+        question: string,
+        answer: string
+    }
+
+    const FaqValue: Array<FaqProps> = [
+        {
+            id: 1,
+            question: "Why Kangs Tour?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            id: 2,
+            question: "How To Book Our Packages?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            id: 3,
+            question: "How Much?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            id: 4,
+            question: "Who Creates Kangs Tour?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+    ]
+
+
+    return (
+        <>
+            <Accordion defaultActiveKey="0">
+                {FaqValue.map((faq, index) => {
+                    const { question, answer } = faq
+                    return (
+                        <Accordion.Item eventKey={`${index}`}>
+                            <Accordion.Header>{question}</Accordion.Header>
+                            <Accordion.Body>
+                                {answer}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    )
+                })}
+            </Accordion>
+        </>
+    )
+}
+
+export default Accordions
--- src/app/components/FAQ.tsx
+++ src/app/components/FAQ.tsx
@@ -3,8 +3,10 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
 import React from 'react'
 import Tags from './Tags';
+import Accordions from './Accordions';
 
 const FAQ = () => {
+
     return (
         <div className='faq'>
             <Container className='faq__container'>
@@ -14,36 +16,10 @@ const FAQ = () => {
                     <span className="faq__subtitle">
                         We've compiled a list of commonly asked questions to provide you with quick and informative answers
                     </span>
-
                 </div>
-                <Accordion defaultActiveKey="0">
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header>Accordion Item #1</Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="1">
-                        <Accordion.Header>Accordion Item #2</Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion>
+                <Accordions />
             </Container>
-        </div>
+        </div >
     )
 }
 
