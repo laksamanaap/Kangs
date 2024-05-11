f9025b56494f2cc7d3b6bb75138a41975028d11e
laksamanaap <laksamana.arya1412@gmail.com>
2024-05-12 00:28:55 +0700

    feat : create send message wa in cta function

--- src/app/components/CTA.tsx
+++ src/app/components/CTA.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Image from 'next/image';
 import bromoImage from '@/assets/bromo/bromo-1.jpg';
 import sendMessageIcon from '@/assets/icons/send-message.svg'
 
 const CTA = () => {
+    const [messageValue, setMessageValue] = useState('');
     return (
         <div className='cta'>
             <Container>
@@ -18,8 +20,11 @@ const CTA = () => {
                         <div className="cta__form-wrapper">
                             <div className="cta__form">
                                 <div className='cta__form-message'>
-                                    <input type="text" />
-                                    <Image src={sendMessageIcon} alt="Send Message" className='cta__form-message-button' />
+                                    <input type="text" onChange={(event) => setMessageValue(event.target.value)} />
+                                    <Image src={sendMessageIcon} alt="Send Message" className='cta__form-message-button' onClick={() => {
+                                        const whatsappLink = `https://api.whatsapp.com/send/?phone=6281230664252&text=${encodeURIComponent(messageValue)}`;
+                                        window.open(whatsappLink);
+                                    }} />
                                     <span className='cta__form-summary'>Embark on your adventure by sending us a message!</span>
                                 </div>
                             </div>
